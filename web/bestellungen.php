330bf2de411e6a1ef473b2f519317022ca279351
Jensssssss <113096719+Jensssssss@users.noreply.github.com>
2025-06-03 09:32:38 +0200

    30 sek timer

--- web/bestellungen.php
+++ web/bestellungen.php
@@ -15,6 +15,8 @@ $result = $conn->query($sql);
 <html lang="de">
 <head>
     <meta charset="UTF-8">
+    <meta http-equiv="refresh" content="30">
+
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Kali's Burgerbude</title>
     <link rel="stylesheet" href="css/bestellungen.css">
